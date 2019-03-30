f9c1549991856329ca5903f6c6e735d3d21f135b
Denny Fauzan <dennyfauzan92@gmail.com>
2019-03-30 20:29:37 +0700

    :hammer: initial Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from './components/header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h1 className="fs-sm fc-primary">Hello World</h1>
       </div>
     );
---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header id="header">
+        <nav className="navbar">
+          <a className="navbar-brand" href="/">
+            asd
+          </a>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
