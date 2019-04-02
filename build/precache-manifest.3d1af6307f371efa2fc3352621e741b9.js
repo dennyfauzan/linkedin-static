809c2090b8b0a031aaceb07c80a5a76ec7ccddb6
Denny Fauzan <dennyfauzan92@gmail.com>
2019-04-02 18:54:37 +0700

    :sparkles: add build files to github

---
+++ build/precache-manifest.3d1af6307f371efa2fc3352621e741b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad1d4f5ba09b69d7fb67",
+    "url": "/static/css/main.28cd5944.chunk.css"
+  },
+  {
+    "revision": "ad1d4f5ba09b69d7fb67",
+    "url": "/static/js/main.c6bc05cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1571311bf69504c670ec",
+    "url": "/static/js/2.7112f108.chunk.js"
+  },
+  {
+    "revision": "8de0dd9a4c799fafc6a5bc92e47f920a",
+    "url": "/index.html"
+  }
+];
