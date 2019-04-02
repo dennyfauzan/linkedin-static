1014a6f948560d4aaaa300467978698db5b36a71
Denny Fauzan <dennyfauzan92@gmail.com>
2019-04-02 18:07:56 +0700

    :hammer: SearchResult

--- src/components/header/SearchBar.js
+++ src/components/header/SearchBar.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { IoIosSearch } from 'react-icons/io';
+import { FaUserFriends, FaBriefcase, FaRegNewspaper } from 'react-icons/fa';
+import CircleImg from '../../components/CircleImg';
 
 class SearchBar extends Component {
   state = {
     isSearchActive : false,
+    recentSearch: ['Denny Fauzan', 'Budi Santoso', 'Dilan Rosada', 'Michael Roodwod', 'Jufi Latiad']
   }
   onSearch = () => {
     this.setState({
@@ -15,6 +18,53 @@ class SearchBar extends Component {
     this.setState({
       isSearchActive: false,
     })
+  }
+
+  recentSearchPeople = () => {
+    return (
+      this.state.recentSearch.map((people, i) => (
+        <div className="each-people px-3 py-2" key={`${people}${i}`}>
+          <CircleImg
+            width="40px"
+            height="40px"
+            src="http://i.pravatar.cc/144"
+            alt="avatar"
+            borderWidth="0"
+          />
+          <p className="fs-md font-medium-bold ml-3 mb-0">{people}</p>
+        </div>
+    ) 
+    ))
+  }
+
+  seachResults = () => {
+    if (this.state.isSearchActive) {
+      return (
+        <div className="search-results">
+          <div className="search-category">
+            <p className="fs-md font-medium-bold px-3 py-2">Search For</p>
+            <div className="cat people px-3 py-2">
+              <FaUserFriends />
+              <p className="fs-md ml-3 mb-0">People</p>
+            </div>
+            <div className="cat jobs px-3 py-2">
+              <FaBriefcase />
+              <p className="fs-md ml-3 mb-0">Jobs</p>
+            </div>
+            <div className="cat content px-3 py-2">
+              <FaRegNewspaper />
+              <p className="fs-md ml-3 mb-0">Content</p>
+            </div>
+          </div>
+          <div className="search-recent">
+            <p className="fs-md font-medium-bold mb-2 px-3 py-2">Recent</p>
+            <div className="recent-peoples">
+              { this.recentSearchPeople() }
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -34,6 +84,7 @@ class SearchBar extends Component {
             </div>
           </div>
         </form>
+        {this.seachResults()}
       </div>
     );
   }
